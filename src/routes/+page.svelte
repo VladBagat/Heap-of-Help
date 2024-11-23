44c58a141901dd2d7a86a5963ff5e5ba47104543
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-11-23 15:38:58 +0000

    Svelte template upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="js">
+    import { goto } from '$app/navigation';
+      goto('/index');
+</script>
+
+<!--This is an entry point. It redirects user to the index page-->
---
+++ src/routes/index/+page.svelte
@@ -0,0 +1,17 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button on:click={navigateToNewRoute()}>
+	Go to Login page
+</button>
+
+<script lang="js">
+    import { goto } from '$app/navigation';
+  
+    function navigateToNewRoute() {
+      goto('/login');
+    }
+</script>
+
+<!--This is an index page. It has HTML button and text + a JavaScript function.
+JS function uses SveltKit's "goto" to redirect user to the login page -->
