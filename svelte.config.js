f29ef77e5ba39495312e4a9b4d92f1a6b1fde54e
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-12-02 21:35:12 +0000

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()  // No need to specify 'runtime' here
+    adapter: adapter({
+      runtime: 'nodejs20.x', 
+    })
   }
 };
 
