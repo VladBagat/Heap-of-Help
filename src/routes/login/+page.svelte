d378468de03c4d3b52ae2cc14c9f12b343ce2cc1
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-12-02 20:29:44 +0000

    Added basic api support

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,14 @@
+<script>
+    let message = '';
+
+    async function fetch_msg(){
+        const response = await fetch('https://hoh-api-24174ce192a4.herokuapp.com/hello')
+        message = await response.text()
+    };
+</script>
+
 <h1>This is a Login page</h1>
+
+<h2>{message}</h2>
+
+<button on:click={fetch_msg}>Click to get a message!</button>
