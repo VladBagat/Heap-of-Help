1a7da1af19da61be5b14f7be951739fce47dfdb5
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-12-02 22:20:19 +0000

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="js">
+    import { goto } from '$app/navigation';
+    import { browser } from '$app/environment';
+
+    if (browser){
+      goto('/index');
+    }
+      
+</script>
+
+<!--This is an entry point. It redirects user to the index page-->
