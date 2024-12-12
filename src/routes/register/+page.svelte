3a15ebe78d689b34de8482969ab1294887772c2e
SamUja04 <98704577+SamUja04@users.noreply.github.com>
2024-12-12 14:24:57 +0000

    Password Validation
    + Password validation for length
    + Confirm password field added
    + Added toggles to view password as plain text
    + Assets that could be used for password visibility by the front end team
    +set api call to register
    +Fixed typo wiht pass validation

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="js">
-    
     let login = $state('');
     let password = $state('');
     let confirm_password = $state('');
@@ -9,29 +8,54 @@
         console.log(login);
         console.log(password);
         console.log(confirm_password)
+        // If pass not valid, user isn't registered
+        let valid = Password_Validation(password, confirm_password)
+        if (Password_Validation === ''){
+            document.getElementById("error").innerHTML = ''
+            const payload = { username: login, password: password, confirm_password:confirm_password};
 
-        const payload = { username: login, password: password, confirm_password:confirm_password};
+            const res = await fetch('https://hoh-api-24174ce192a4.herokuapp.com/register', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload),
+            });
 
-        // const res = await fetch('https://hoh-api-24174ce192a4.herokuapp.com/register', {
-        //     method: 'POST',
-        //     credentials: 'include',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(payload),
-        // });
+            const json = await res.json();
 
-        const json = await res.json();
+            console.log(json)
 
-        console.log(json)
-
-        if (json[0].success) {
-            IndexRedirect()
+            if (json[0].success) {
+                IndexRedirect()
+            }
+        }
+        else{
+            console.log(valid);
+            document.getElementById("error").innerHTML = valid;
         }
     }
     
 
     import { goto } from '$app/navigation';
+
+    function Password_Validation(password, confirm_password){
+        if (password.length < 8){
+            return "Minimum password length 8, Try Again.";
+        }
+        if (password !== confirm_password){
+            return "Passwords do not match, Try Again";
+        }       
+        return "";
+    }
+
+    function ToggleVisability(fieldId) {
+        const field = document.getElementById(fieldId);
+        // ? is a ternary operator, basically a one line if statement
+        // If password hidden when the toggle is pressed it wil be shown, otherwise it will be hidden
+        document.getElementById(fieldId).type = (field.type === "password") ? "text" : "password";
+    }
 
     function LoginRedirect(){
         goto('/login');
@@ -46,10 +70,12 @@
 <div id="register-form">
     <div id="container">
         <input class="element" type="text" bind:value={login} placeholder="Username" />
-        <input class="element" type="password" bind:value={password} placeholder="Password" /> 
-        <input class="element" type="password" bind:value={confirm_password} placeholder="Confirm Password" /> 
+        <input id="pass" class="element" type="password" bind:value={password} placeholder="Password" /> 
+        <button class="element" onclick={() => ToggleVisability("pass")}>Toggle Pass Visibility</button>
+        <input  id="conf_pass" class="element" type="password" bind:value={confirm_password} placeholder="Confirm Password" /> 
+        <button class="element" onclick={() => ToggleVisability("conf_pass")}>Toggle Pass Visibility</button>
         <button class="element" onclick={Register}> Register </button>
-        
+        <p id="error"></p>
         <button class="redirect" onclick={LoginRedirect}> Already on Heap of Help? </button>
     </div>
 </div>
