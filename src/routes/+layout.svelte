9a52e607e32cd3bf23b1eeeb83560d737ede8c3a
JackWood <jack.wood-4@student.manchester.ac.uk>
2025-01-30 15:36:49 +0000

    Created some placeholders for future pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+
+  
+  <header class="header">
+    <div class="container">
+      <a href="/" class="logo">Heap of Help</a>
+      
+      <nav class="nav">
+        <a href="/" class="nav-link">Page....</a>
+        <a href="/" class="nav-link">Page</a>
+        <a href="/login" class="nav-link">Login</a>
+      </nav>
+      
+      <!-- Mobile menu toggle -->
+
+    </div>
+  </header>
+  
+  <main>
+    <slot />
+  </main>
+
+  
