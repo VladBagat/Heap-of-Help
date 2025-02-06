b202fc4f6d30d6a998621a6ee08cf25cec345d79
Sam <98704577+SamUja04@users.noreply.github.com>
2025-02-06 13:02:44 +0000

    Discover Page

---
+++ src/routes/discovery/+page.svelte
@@ -0,0 +1,7 @@
+<main>
+  <h1 class="header">Discovery</h1>
+  <p>Discover Help</p>
+  
+</main>
+<!--This is an index page. It has HTML button and text + a JavaScript function.
+JS function uses SveltKit's "goto" to redirect user to the login page -->
