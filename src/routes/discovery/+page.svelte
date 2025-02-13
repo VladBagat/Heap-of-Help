f0e8b4f1b78db7e4a5313cba6b419c341e01fb3a
SamUja04 <98704577+SamUja04@users.noreply.github.com>
2025-02-13 11:04:33 +0000

    Updated Grid Styles
    Made cards look nicer
    Improved responsiveness

--- src/routes/discovery/+page.svelte
+++ src/routes/discovery/+page.svelte
@@ -9,7 +9,7 @@
     {
       name: "Jane Smith",
       description:
-        "A passionate graphic designer specializing in digital art and branding.",
+        "A passionate graphic designer specializing in digital art and branding.A passionate graphic designer specializing in digital art and branding.A passionate graphic designer specializing in digital art and branding.A passionate graphic designer specializing in digital art and branding.A passionate graphic designer specializing in digital art and branding.A passionate graphic designer specializing in digital art and branding.A passionate graphic designer specializing in digital art and branding.A passionate graphic designer specializing in digital art and branding.",
       tags: ["Python", "Senior", "Sigma"],
     },
     {
@@ -83,41 +83,15 @@
   <h1 class="main-header">Discovery</h1>
   <h2 class="category">Recommended</h2>
   <div class="main-grid-container">
-    <!-- <div class="box">a</div>
-    <div class="box">b</div>
-    <div class="box">c</div>
-    <div class="box">d</div>
-    <div class="box">e</div>
-    <div class="box">f</div> -->
     {#each temp as tile}
-      <div class="box">
+      <div class="card">
         <div class="card-image">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            width="175"
-            height="175"
-          >
-            <defs>
-              <mask id="mask">
-                <ellipse
-                  cx="50%"
-                  cy="50%"
-                  rx="50%"
-                  ry="50%"
-                  fill="white"
-                  filter="url(#filter)"
-                ></ellipse>
-              </mask>
-            </defs>
-
-            <image
-              xlink:href="https://picsum.photos/id/1005/400/300"
-              width="150"
-              height="150"
-              mask="url(#mask)"
-            ></image>
-          </svg>
+          <img
+          src="https://picsum.photos/id/1005/400/300"
+          width="150"
+          height="150"
+          alt="Profile"
+        />
         </div>
         <div class="description">{tile.description}</div>
         <div class="name">{tile.name}</div>
@@ -131,36 +105,15 @@
   </div>
   <h2 class="category">Most Popular</h2>
   <div class="main-grid-container">
-
     {#each temp as tile}
-      <div class="box">
+      <div class="card">
         <div class="card-image">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            width="175"
-            height="175"
-          >
-            <defs>
-              <mask id="mask">
-                <ellipse
-                  cx="50%"
-                  cy="50%"
-                  rx="50%"
-                  ry="50%"
-                  fill="white"
-                  filter="url(#filter)"
-                ></ellipse>
-              </mask>
-            </defs>
-
-            <image
-              xlink:href="https://picsum.photos/id/1005/400/300"
-              width="150"
-              height="150"
-              mask="url(#mask)"
-            ></image>
-          </svg>
+          <img
+          src="https://picsum.photos/id/1005/400/300"
+          width="150"
+          height="150"
+          alt="Profile"
+        />
         </div>
         <div class="description">{tile.description}</div>
         <div class="name">{tile.name}</div>
@@ -184,7 +137,7 @@
     font-size: clamp(1.5rem, 2.5vw, 3rem);
     font-weight: 600;
     color: #363434;
-    padding: 10px 7px 0;
+    padding: 10px 7px 0px;
     margin-bottom: 10px;
     border-bottom: 1px solid #ececec;
     font-family: sans-serif;
@@ -203,56 +156,71 @@
   .main-grid-container {
     color: #000;
     display: grid;
-    grid-template-columns: 1fr;
-    gap: 0.5rem;
-    place-content: center;
-    padding: 0px 0px 5px;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 400px));
+    row-gap: 1.5rem;
+    column-gap: 0.5rem;
+  
+    justify-content: center;
+    margin-bottom: 10px;
   }
-  .box {
-    width: 92%;
-    height: 300px;
+  .card {
+    width: 90%;
+    height: 250px;
     font-family: "Arial";
     padding: 1rem;
     cursor: pointer;
     border-radius: 0.75rem;
     background: #f7f7f8;
     box-shadow: 0px 8px 16px 0px rgb(0 0 0 / 3%);
-
+    place-content: center;
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-template-rows: repeat(2, 1fr);
     grid-column-gap: 0.2rem;
     grid-row-gap: 0px;
   }
-  .box:hover {
-    box-shadow: 0px 8px 16px 0px rgb(0 0 0 / 10%);
+  .card:hover {
+    box-shadow: 0px 8px 16px 0px hwb(0 0% 100% / 0.1);
     transform: translateY(-2px);
     transition: all 0.3s ease-in-out;
   }
   .card-image {
-    width: 100px;
-    height: 100px;
-    border-radius: 6px 6px 0 0;
-    border-radius: 50%;
-    margin-right: 15px;
-    mask-image: radial-gradient(circle, black 50%, rgba(0, 0, 0, 0) 50%);
+  grid-column: 1;   
+  grid-row: 1;  
+  /* From W3Schools */
+  mask-image: radial-gradient(circle, black 50%, rgba(0, 0, 0, 0) 50%);
   }
-
-  /* For Small devices (portrait tablets and large phones, 600px and up) */
-  @media only screen and (min-width: 700px) {
-    .main-grid-container {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      gap: 1rem;
-    }
-  }
-
-  /* For Medium and large devices (landscape tablets and desktops, 768px and up) */
-  @media only screen and (min-width: 1200px) {
-    .main-grid-container {
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr;
-      gap: 1.5rem;
-    }
-  }
+  .name {
+  grid-column: 2; 
+  grid-row: 1;     
+  place-content: center;
+}
+  .description {
+  grid-column: 1 / span 2; 
+  grid-row: 2;            
+  max-width: 400px;
+  align-content: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  
+}
+  .tags-container {
+  grid-column: 1 / span 2; 
+  grid-row: 3;             
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+}
+.tag{
+  background-color: #333333;
+  color: #ffffff;
+  padding: 5px 10px;
+  border-radius: 5px;
+  text-transform: capitalize;
+  place-content: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 </style>
