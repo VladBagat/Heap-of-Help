3e0457e2b029f3d0d33bf54d9481f3ea89671047
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2025-02-25 11:19:00 +0000

    Fixed bad endpoint

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -11,7 +11,7 @@
 
         const payload = { username: login, password: password, remember:is_remember };
 
-        const res = await fetch('https://hoh-api-24174ce192a4.herokuapp.com/login', {
+        const res = await fetch('api/login', {
             method: 'POST',
             credentials: 'include',
             headers: {
