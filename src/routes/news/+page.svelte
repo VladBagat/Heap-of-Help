64549714fb0990fd074474f9a6e213d082493758
SamUja04 <98704577+SamUja04@users.noreply.github.com>
2025-02-27 10:31:56 +0000

    Started Work on news page

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+ 		let data = $props();
+    $inspect(data);
+</script>
+
+<div class="news-container">
+  {#each data as article}
+    <div class="article">
+      article
+    </div>
+  {/each}
+</div>
+
