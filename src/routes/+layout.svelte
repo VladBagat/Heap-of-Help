9d53c1cc4a261f484be74e1cd23e636d871013ff
JackWood <jack.wood-4@student.manchester.ac.uk>
2025-03-11 19:14:02 +0000

    Fixed logout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,14 @@
-<script>
+<script lang="js">
     let menuOpen = false;
     import "../styles.css";
     import { onMount } from "svelte";
     import { goto } from "$app/navigation";
-    let isLoggedIn = false;
+    import { isLoggedIn, checkAuth, logout } from "../auth.js";
 
-    async function checkAuth() {
-        try {
-            const res = await fetch('api/auth', {
-                method: 'GET',
-                credentials: 'include',
-            });
-
-            if (res.ok) {
-                const data = await res.json();
-                isLoggedIn = true; // User is authenticated
-            } else {
-                isLoggedIn = false; // User is not authenticated
-            }
-        } catch (error) {
-            console.error('Error fetching auth:', error);
-            isLoggedIn = false;
-        }
-    }
-
-    async function logout() {
-        await fetch("/logout", { method: "POST", credentials: "include" });
-        isLoggedIn = false;
-        goto("/login");
-    }
-
-    onMount(checkAuth);
-  </script>
+    onMount(() => {
+        checkAuth();
+    }); 
+</script>
   
   <header class="header">
     <div class="container">
@@ -40,10 +17,9 @@
       <nav class="nav" class:open={menuOpen}>
         <a href="/" class="nav-link">Tutors</a>
         <a href="/" class="nav-link">Chat</a>
-        {#if isLoggedIn}
-          <a href="/profile" class="nav-link">Profile</a>
-          <a href = "/index" class="nav-link" on:click={logout}>Logout</a>
-        {:else}
+        {#if $isLoggedIn}          <a href="/profile" class="nav-link">Profile</a>
+          <a href="/index" class="nav-link" on:click={logout}>Logout</a>
+          {:else}
           <a href="/login" class="nav-link">Login</a>
         {/if}
       </nav>
